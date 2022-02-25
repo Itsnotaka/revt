96dfa5efe68423ab2cf288461a68fd1a655ad71c
Aka <itsnotaka@Gmail.com>
2022-02-25 16:15:30 -0500

    lint

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -15,11 +15,6 @@ app.on('second-instance', restoreOrCreateWindow);
 
 
 /**
- * Disable Hardware Acceleration for more power-save
- */
-app.disableHardwareAcceleration();
-
-/**
  * Shout down background process if all windows was closed
  */
 app.on('window-all-closed', () => {
@@ -41,20 +36,6 @@ app.whenReady()
   .then(restoreOrCreateWindow)
   .catch((e) => console.error('Failed create window:', e));
 
-
-/**
- * Install Vue.js or some other devtools in development mode only
- */
-if (import.meta.env.DEV) {
-  app.whenReady()
-    .then(() => import('electron-devtools-installer'))
-    .then(({default: installExtension, REACT_DEVELOPER_TOOLS}) => installExtension(REACT_DEVELOPER_TOOLS, {
-      loadExtensionOptions: {
-        allowFileAccess: true,
-      },
-    }))
-    .catch(e => console.error('Failed install extension:', e));
-}
 
 /**
  * Check new app version in production mode only
