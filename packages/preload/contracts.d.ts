e05a5c06791a6e7db8d3c6d1e18f1451e6418cee
Aka <itsnotaka@Gmail.com>
2022-02-26 00:12:20 -0500

    Update for better looking mainWorld expose

--- packages/preload/contracts.d.ts
+++
@@ -1,6 +0,0 @@
-interface Exposed {
-	readonly nodeCrypto: Readonly<typeof import('./src/nodeCrypto').nodeCrypto>;
-	readonly versions: Readonly<typeof import('./src/versions').versions>;
-}
-
-interface Window extends Exposed {}
--- packages/preload/src/versions.ts
+++
@@ -1,6 +0,0 @@
-import {exposeInMainWorld} from './exposeInMainWorld';
-
-// Export for types in contracts.d.ts
-export const {versions} = process;
-
-exposeInMainWorld('versions', versions);
