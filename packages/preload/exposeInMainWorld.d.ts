df23cd7dad413693509738b7e7cc8937553db768
Aka <itsnotaka@Gmail.com>
2022-02-26 17:06:37 -0500

    Tsconfig fix for error

--- packages/preload/exposeInMainWorld.d.ts
+++ packages/preload/exposeInMainWorld.d.ts
@@ -1,11 +1,9 @@
-declare global {
-	interface Window {
-		readonly revt: { version: number };
-		/**
-		 * Safe expose node.js API
-		 * @example
-		 * window.nodeCrypto('data')
-		 */
-		readonly nodeCrypto: { sha256sum: any };
-	}
+interface Window {
+	readonly revt: { version: number };
+	/**
+	 * Safe expose node.js API
+	 * @example
+	 * window.nodeCrypto('data')
+	 */
+	readonly nodeCrypto: { sha256sum: any };
 }
