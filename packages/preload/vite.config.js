8ab22833c2c34afd7152327bb85c457984ed2807
Aka <itsnotaka@Gmail.com>
2022-02-26 00:14:27 -0500

    vite config update for rollup

--- packages/preload/vite.config.js
+++ packages/preload/vite.config.js
@@ -1,5 +1,6 @@
-import {chrome} from '../../.electron-vendors.cache.json';
-import {builtinModules} from 'module';
+import {chrome} from "../../.electron-vendors.cache.json";
+import {join} from "path";
+import {builtinModules} from "module";
 
 const PACKAGE_ROOT = __dirname;
 
@@ -11,23 +12,28 @@ const config = {
   mode: process.env.MODE,
   root: PACKAGE_ROOT,
   envDir: process.cwd(),
+  resolve: {
+    alias: {
+      "/@/": join(PACKAGE_ROOT, "src") + "/",
+    },
+  },
   build: {
-    sourcemap: 'inline',
+    sourcemap: "inline",
     target: `chrome${chrome}`,
-    outDir: 'dist',
-    assetsDir: '.',
-    minify: process.env.MODE !== 'development',
+    outDir: "dist",
+    assetsDir: ".",
+    minify: process.env.MODE !== "development",
     lib: {
-      entry: 'src/index.ts',
-      formats: ['cjs'],
+      entry: "src/index.ts",
+      formats: ["cjs"],
     },
     rollupOptions: {
       external: [
-        'electron',
+        "electron",
         ...builtinModules.flatMap(p => [p, `node:${p}`]),
       ],
       output: {
-        entryFileNames: '[name].cjs',
+        entryFileNames: "[name].cjs",
       },
     },
     emptyOutDir: true,
